dbb88b7796b3d1593cfd7c685be25733391d125f
qianduanxiaoliu <2461362522@qq.com>
2022-05-21 10:15:08 +0800

    完成了首页功能

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -3,4 +3,20 @@
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
 $.ajaxPrefilter((option) => {
 	option.url = 'http://www.liulongbin.top:3007' + option.url;
+	//在请求之前给需要权限的注入 token
+	if (option.url.includes('/my/')) {
+		option.headers = {
+			Authorization: localStorage.getItem('token')
+		};
+	}
+
+  // 统一处理权限问题
+	option.complete = (res) => {
+		console.log(res);
+    if (res.responseJSON.status == 1 && res.responseJSON.message == '身份认证失败！') {
+      // 清空token 并跳转页面
+			localStorage.removeItem('token');
+			location.href = '/login.html';
+		}
+	};
 });
--- assets/js/index.js
+++ assets/js/index.js
@@ -1,21 +1,40 @@
 $(function () {
 	getUserInfo();
+
+	//点击退出请求
+	$('#btnLogout').click(() => {
+		layer.confirm('确认是否退出', { icon: 3, title: '提示' }, function () {
+			//do something
+			localStorage.removeItem('token');
+			location.href = '/login.html';
+		});
+	});
 });
 
+//获取用户信息
 function getUserInfo() {
 	$.ajax({
 		type: 'GET',
 		url: '/my/userinfo',
-		headers: {
-			Authorization: localStorage.getItem('token')
-		},
+		// headers: {
+		// 	Authorization: localStorage.getItem('token')
+		// },
 		success: function (res) {
 			console.log(res);
 			if (res.status !== 0) return layui.layer.msg('请求数据失败');
-      layui.layer.msg('请求数据成功');
-      //调用渲染头像函数
+			layui.layer.msg('请求数据成功');
+			//调用渲染头像函数
 			randerAvatar(res.data);
 		}
+
+		//不论成功还是失败，最终都会调用 complete 回调函数
+		// complete: (res) => {
+		// 	console.log(res);
+		// 	if (res.responseJSON.status == 1 && res.responseJSON.message == '身份认证失败！') {
+		// 		localStorage.removeItem('token');
+		// 		location.href = '/login.html';
+		// 	}
+		// }
 	});
 }
 
